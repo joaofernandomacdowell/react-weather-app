7c16308830dda9b1b6ddc6e92f8003a25105c4d8
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-08 13:45:57 -0200

    Create alias to 'openWeathermap.jsx'

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         WeatherForm: 'app/components/WeatherForm.jsx',
         WeatherMessage: 'app/components/WeatherMessage.jsx',
         About: 'app/components/About.jsx',
-        Examples: 'app/components/Examples.jsx'
+        Examples: 'app/components/Examples.jsx',
+        openWeathermap: 'app/api/openWeathermap.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
