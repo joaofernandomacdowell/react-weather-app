ba7d62afcd40eb707f73b89ee35532d3d61498eb
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-08 13:45:04 -0200

    Call the API with a promise in 'Weather.jsx' component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeathermap = require('openWeathermap');
 
 var Weather = React.createClass({
   getInitialState: function () {
@@ -10,9 +11,14 @@ var Weather = React.createClass({
     }
   },
   handleSearch: function (location) {
-    this.setState({
-      location: location,
-      temp: 23
+    var that = this;
+    openWeathermap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function (errorMessage) {
+      alert(errorMessage);
     });
   },
   render: function () {
