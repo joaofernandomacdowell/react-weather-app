2d3f6fef8ae046279a0905ea53f5c255f6e6cf3c
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-09 16:57:25 -0200

    Refactor using ES6 arrow functions

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,18 +3,15 @@ var {Link, IndexLink} = require('react-router');
 //Features of Link
 //Customize styles and classes
 
-var Nav = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h2> Nav Component</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      </div>
-
-    );
-  }
-});
+var Nav = () =>  {
+  return (
+    <div>
+      <h2> Nav Component</h2>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+    </div>
+  );
+};
 
 module.exports = Nav;
